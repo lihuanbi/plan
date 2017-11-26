380e4dca13fd5748e2bf4e0901021c8ad0f8e47e
lihuanbi <32891666+lihuanbi@users.noreply.github.com>
2017-11-26 23:42:07 +0800

    Add files via upload

---
+++ js/new_file.js
@@ -0,0 +1,89 @@
+function show() {
+			event.cancelBubble = true;
+			document.getElementById("light").style.display = "block";		
+			return false;
+		}
+
+		function doClose() {
+			document.getElementById("light").style.display = "none";
+		}
+
+		function show2() {
+			event.cancelBubble = true;
+			document.getElementById("light-2").style.display = "block";
+			return false;
+		}
+
+		function doClose2() {
+			document.getElementById("light-2").style.display = "none";
+
+		}
+
+		function show3() {
+			event.cancelBubble = true;
+			document.getElementById("light-3").style.display = "block";
+			return false;
+		}
+
+		function doClose3() {
+			document.getElementById("light-3").style.display = "none";
+		}
+
+		function show4() {
+			event.cancelBubble = true;
+			document.getElementById("light-4").style.display = "block";
+			return false;
+		}
+
+		function doClose4() {
+			document.getElementById("light-4").style.display = "none";
+			document.getElementById("fade").style.opacity = "1";
+		}
+
+		function show5() {
+			event.cancelBubble = true;
+			document.getElementById("light-5").style.display = "block";
+
+			return false;
+		}
+
+		function doClose5() {
+			document.getElementById("light-5").style.display = "none";
+			document.getElementById("fade").style.opacity = "1";
+		}
+
+		function show6() {
+			event.cancelBubble = true;
+			document.getElementById("light-6").style.display = "block";
+
+			return false;
+		}
+
+		function doClose6() {
+			document.getElementById("light-6").style.display = "none";
+			document.getElementById("fade").style.opacity = "1";
+		}
+
+		function show7() {
+			event.cancelBubble = true;
+			document.getElementById("light-7").style.display = "block";
+
+			return false;
+		}
+
+		function doClose7() {
+			document.getElementById("light-7").style.display = "none";
+			document.getElementById("fade").style.opacity = "1";
+		}
+
+		function show8() {
+			event.cancelBubble = true;
+			document.getElementById("light-8").style.display = "block";
+
+			return false;
+		}
+
+		function doClose8() {
+			document.getElementById("light-8").style.display = "none";
+			document.getElementById("fade").style.opacity = "1";
+		}
